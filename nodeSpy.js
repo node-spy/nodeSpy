355178425e0455122296a1008c27af1afef3ece4
Eviscerare <edward.h.guo@gmail.com>
2016-10-28 22:50:01 -0700

    commented for clarification

--- nodeSpy.js
+++ nodeSpy.js
@@ -2,13 +2,19 @@ const express = require('express');
 
 const app = express();
 
+// set routes for Node Spy to serve client's express route data'
 app.get('/nodeSpy', (req, res, next) => {
   res.sendFile(__dirname + '/index.html');
 });
+app.post('/getCache', (req, res, next) => {
+  res.end(JSON.stringify(nodeSpy.cache));
+});
 
+// set port during initial server call, for use in redirect
 let PORT;
 
 const nodeSpy = {
+  // cache for pushing spy data to
   cache: [],
   server: (port) => {
     PORT = port;
@@ -16,10 +22,12 @@ const nodeSpy = {
       console.log('nodeSpy connected to port ' + port);
     });
   },
+  // log request and response data from user determined middleware point
   spy: (req, res, next) => {
     nodeSpy.cache.push({ Method: req.method, URL: req.url });
     next();
   },
+  // redirect and serve data
   report: (req, res, next) => {
     console.log(nodeSpy.cache);
     res.redirect(`http://localhost:${PORT}/nodeSpy`);
